78d33cfb5fd733f3a5e87bcc6a295eb4dc4b9a5e
rguptaquantum <50578248+rguptaquantum@users.noreply.github.com>
2019-05-29 09:56:10 +0530

    Duplicate code for csrf

--- src/main/java/com/rguptaquantum/fabwallet/security/WebSecurityConfig.java
+++ src/main/java/com/rguptaquantum/fabwallet/security/WebSecurityConfig.java
@@ -23,9 +23,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        // Disable CSRF (cross site request forgery)
-        http.csrf().disable();
-
         // No session will be created or used by spring security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
